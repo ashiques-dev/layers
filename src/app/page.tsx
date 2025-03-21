24da44b7268acb6c3020b42fdc4aa3701715f976
ashiques-dev <ashiques.dev@gmail.com>
2025-03-21 20:17:33 +0530

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import Navbar from '@/sections/navbar';
 import React from 'react'
 
 const Home = () => {
   return (
     <>
-    
+      <Navbar />
     </>
-  )
+  );
 }
 
 export default Home
